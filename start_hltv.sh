5161344bd273c159a17135945ec9563db61be126
R4 <R4@users.noreply.github.com>
2026-09-17 20:13:16 +0330

    فیکس باگ مشکل دار

--- start_hltv.sh
+++ start_hltv.sh
@@ -6,6 +6,11 @@ cd "$(dirname "$0")"
 # Ensure the port 27015 matches your server port.
 # Screen is recommended so it stays alive in the background.
 
+SERVER_IP=$(hostname -I | awk '{print $1}')
+if [ -z "$SERVER_IP" ]; then
+    SERVER_IP="127.0.0.1"
+fi
+
 echo "Starting GAMELAND HLTV..."
-screen -A -m -d -S gameland_hltv ./hltv +connect 127.0.0.1:27015 +exec hltv.cfg
+screen -A -m -d -S gameland_hltv ./hltv +connect ${SERVER_IP}:27015 +exec hltv.cfg
 echo "HLTV started in screen session 'gameland_hltv'."
